e0a2d1030ca533e7730cafbd1ad9fc72868997b5
Md Atiquer Rahman <35799095+AtiQ-Rahman@users.noreply.github.com>
2018-04-17 09:56:32 +0600

    Zombie add

--- ZoomBie Game/src/mainPackage/MainClass.java
+++ ZoomBie Game/src/mainPackage/MainClass.java
@@ -1,27 +1,154 @@
 package mainPackage;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
 
 public class MainClass {
 
+	private   JFrame frame;
+	private  JLabel background_1;
+	private  JLabel background_2;
+	private  JPanel jp_1;
+	
+	public MainClass() {
+		
+		initialize();
+	}
 	 
 	public static void main(String[] args) throws IOException{
 		
-		Zombie z1 = new Zombie();
+		MainClass mainclass=new MainClass();
+		
+	}
+	
+	public void initialize() {
+		
+		JPanel jp=null;
+		JLabel background=null;
+		BufferedImage img;
+		
+		frame = new JFrame();
+		frame.setBounds(100, 100, 384, 521);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(true);
+		
+		try
+		{
+			
+		 img = ImageIO.read(getClass().getResourceAsStream("/images4.jpg")); 
+		 jp_1=new JPanel() {
+			 
+			 	@Override
+			    public void paintComponent(Graphics g){
+			       
+			        g.drawImage(img, 0, 0,468,600, this);
+			        
+			    }
+		 };
+		 
+		
+		 
+		
+		 
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+	
+		frame.getContentPane().add(jp_1);
+		
+		JButton btnNewButton_1 = new JButton("New Game");
+		btnNewButton_1.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btnNewButton_1.setBackground(new Color(153, 255, 51));
+		btnNewButton_1.setBounds(105, 160, 121, 32);
+		btnNewButton_1.addActionListener(new ActionListener() {
+			
+				public void actionPerformed(ActionEvent e) {
+					
+					
+					frame.setVisible(false);
+					
+					
+					JFrame f1 = new JFrame("<--SPL-->"); //frame er object nilam
+					
+					Zombie z1 = new Zombie(f1);
+					f1.setSize(1300,700);
+					f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+					f1.add(z1);
+					f1.setVisible(true);
+					
+					
+					
+					f1.setResizable(true);
+				
+				}
+				
+		});
+		jp_1.setLayout(null);
+		jp_1.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Statistics");
+		btnNewButton_2.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btnNewButton_2.setBackground(new Color(0, 255, 0));
+		btnNewButton_2.setBounds(115, 205, 100, 32);
+		jp_1.add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("Help");
+		btnNewButton_3.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btnNewButton_3.setBackground(new Color(51, 255, 51));
+		btnNewButton_3.setBounds(125, 250, 78, 32);
+		jp_1.add(btnNewButton_3);
+		
+		JButton btnExit = new JButton("Exit");
+		btnExit.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btnExit.setBackground(Color.RED);
+		btnExit.setBounds(125, 295, 78, 32);
+		jp_1.add(btnExit);
+		
+		btnExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				
+				frame.setVisible(false);
+			}
+		});
 		
 		
-		JFrame f1 = new JFrame("<--SPL-->"); //frame er object nilam
-		f1.setSize(1300,675);
-		f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f1.add(z1);
-		f1.setVisible(true);
 		
-		f1.setResizable(false);
+		
+		
+		
+		//background.setLayout(new FlowLayout());
+		/*JLabel l1=new JLabel("Here is a button");
+		JButton b1=new JButton("I am a button");
+		frame.getContentPane().add(background);
+		background.add(l1);
+		background.add(b1);*/
+		//frame.setSize(new Dimension(img.getWidth(null), img.getHeight(null)));
+
+		
+		frame.setVisible(true);
+		
+		
+		
 		
 	
-		
+
 		
 				
 	}
--- ZoomBie Game/src/mainPackage/Zombie.java
+++ ZoomBie Game/src/mainPackage/Zombie.java
@@ -1,10 +1,22 @@
 package mainPackage;
 
+
+import java.io.File;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
 import java.awt.Image;
+import java.awt.Menu;
+import java.awt.MenuBar;
+import java.awt.MenuItem;
+import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.Window;
 import java.awt.event.ActionEvent;
@@ -14,15 +26,31 @@ import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 //import java.util.Timer;
 import javax.imageio.ImageIO;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.Timer;
+import javax.swing.UIManager;
+import javax.swing.plaf.FontUIResource;
+
+
 
 public class Zombie extends JPanel implements ActionListener,KeyListener  {
 	
@@ -38,11 +66,49 @@ public class Zombie extends JPanel implements ActionListener,KeyListener  {
 	public int count=0;
 	boolean intersects=false;
 	
+	int[][] arrZombie= {{150,540,1},{800,435,1},{1000,330,1},{1195,225,1},{100,120,1},{5,15,1}};
+	
+	int[] dirValue= {-5,-3,-2,-1,-4,-3};
+	
+	boolean shoot=false;
+	
+	ArrayList<Integer> bullet=new ArrayList<Integer>();
+	int[] difference= {5,40,75,110,145};
 
 	
-	public Zombie()  {
+	public Zombie(JFrame frame)  {
 		
 		
+		MenuBar mn=new MenuBar();
+		Menu menu=new Menu("Menu");
+		menu.setFont(new Font("thoma",Font.BOLD,16));
+		mn.setFont(new Font("thoma",Font.BOLD,16));
+
+		
+		MenuItem item1=new MenuItem("New Game");
+		MenuItem item2=new MenuItem("Highest Score");
+		MenuItem item3=new MenuItem("Exit");
+		
+		menu.add(item1);
+		menu.add(item2);
+		menu.add(item3);
+		
+		mn.add(menu);
+		
+		frame.setMenuBar(mn);
+		
+		item3.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				frame.setVisible(false);
+				 
+				
+			}
+			
+		});
+
 		
 		setPreferredSize(new Dimension(1360,675)); 
 		addKeyListener(this);
@@ -89,31 +155,89 @@ public class Zombie extends JPanel implements ActionListener,KeyListener  {
 		
 		
 		
+		
+		item1.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+				Random randData = new Random();
+				
+				for(int i=0;i<5;i++) {
+					arrStar[i][0] = randData.nextInt(1200);
+					arrStar[i][1]=615;
+				}
+
+				for(int i=5;i<10;i++) {
+					arrStar[i][0] = randData.nextInt(1200);
+					arrStar[i][1]=510;
+				}		
+				
+				for(int i=10;i<15;i++) {
+					arrStar[i][0] = randData.nextInt(1200);
+					arrStar[i][1]=405;
+				}
+				
+				for(int i=15;i<20;i++) {
+					arrStar[i][0] = randData.nextInt(1200);
+					arrStar[i][1]=300;
+				}
+				
+				for(int i=20;i<25;i++) {
+					arrStar[i][0] = randData.nextInt(1200);
+					arrStar[i][1]=195;
+				}
+				
+				for(int i=25;i<30;i++) {
+					arrStar[i][0] = randData.nextInt(1000);
+					arrStar[i][1]=90;
+				}
+				
+				
+				for(int i=0;i<30;i++) {
+					
+					arrStar[i][2]=1;
+				}
+				
+				
+				
+			}
+			
+		});
+		
 
 		
-		try {
-		img1=ImageIO.read(new File("C:\\Users\\Atiq\\Downloads\\images\\image1.jpg"));
-		
-		img2=ImageIO.read(new File("C:\\Users\\Atiq\\Downloads\\images\\image2.png"));
-		
-		img3=ImageIO.read(new File("C:\\Users\\Atiq\\Downloads\\images\\image3.png"));
-		
-		
-		//img4=ImageIO.read(getClass().getResourceAsStream("/girl.png"));
-
-	}catch(IOException e) {
-		
-		e.printStackTrace();
-
-		
-		
-		
-	}
+			try {
+			img1=ImageIO.read(getClass().getResourceAsStream("/a2.jpg"));
+			
+			img2=ImageIO.read(getClass().getResourceAsStream("/b.png"));
+			
+			img3=ImageIO.read(getClass().getResourceAsStream("/a1.png"));
+			
+			img4=ImageIO.read(getClass().getResourceAsStream("/atiq.png"));
+			
+			
+			
+			
+			//img4=ImageIO.read(getClass().getResourceAsStream("/girl.png"));
+	
+		}catch(IOException e) {
+			
+			e.printStackTrace();
+	
+			
+			
+				
+		}
 	
 		timer=new Timer(delay,this);
 		timer.start();
 		
 	}
+	
+	
+
 	
 	public void paintComponent(Graphics g) {
 		
@@ -128,15 +252,15 @@ public class Zombie extends JPanel implements ActionListener,KeyListener  {
 			//stair1
 			for(int i=105;i<=210;i=i+21) {
 				
-				g.fillRect(100,i,50,5);
+				g.fillRect(650,i,50,5);
 				
 			}
 		
 			
 			g.setColor(Color.red);
 			
-			g.fillRect(100,105,5, 105);
-			g.fillRect(150,105,5, 105);
+			g.fillRect(650,105,5, 105);
+			g.fillRect(700,105,5, 105);
 			
 
 			//stair2
@@ -229,6 +353,9 @@ public class Zombie extends JPanel implements ActionListener,KeyListener  {
 			
 			
 	
+		
+			
+			
 			
 			g.drawImage(img3,x,y,40,60,this);
 		
@@ -241,8 +368,38 @@ public class Zombie extends JPanel implements ActionListener,KeyListener  {
 			g.setFont(new Font("serif",Font.BOLD,24));
 			g.drawString("Score is: "+count,550,50);
 			
+			for(int i=0;i<6;i++) {
+				
+				if(arrZombie[i][2]==1) g.drawImage(img4,arrZombie[i][0],arrZombie[i][1],100,100, this);
+			}
+			
+			
+			
+			
+			for(int x:bullet) {
+				
+				g.setColor(Color.RED);
+				g.fillRoundRect(x, y+25,15,5,3,3);
+			}
+			
+			if(shoot==true) {
+				
+				for(int i=0;i<1;i++) {
+					
+					bullet.add(x-difference[i]);
+				}
+				
+				
+				
+				shoot=false;
+				
+			}
+			
+			
 		
 	}
+	
+
 
 	@Override
 	public void keyTyped(KeyEvent e) {
@@ -257,6 +414,8 @@ public class Zombie extends JPanel implements ActionListener,KeyListener  {
 
 		if((e.getKeyCode()==KeyEvent.VK_LEFT)&&x>5) {
 			
+		
+			
 			
 			x=x-5;			
 			
@@ -270,23 +429,49 @@ public class Zombie extends JPanel implements ActionListener,KeyListener  {
 			
 		}
 		
-		if((e.getKeyCode()==KeyEvent.VK_UP)&&((x>100&&x<150&&y>40&&y<355)||(x>240&&x<315&&y>245&&y<560)
-				||(x>500&&x<550&&y>465&&y<670)||(x>500&&x<550&&y>145&&y<460)||(x>900&&x<950&&y>360&&y<670))) {
+		if((e.getKeyCode()==KeyEvent.VK_UP)&&((x>645&&x<680&&y>45&&y<258)||(x>245&&x<265&&y>258&&y<465)
+				||(x>495&&x<515&&y>465&&y<670)||(x>495&&x<520&&y>152&&y<360)||(x>895&&x<920&&y>360&&y<560))) {
 			
 		
 			y=y-5;
 			
 		}
 		
-		if((e.getKeyCode()==KeyEvent.VK_DOWN)&&((x>100&&x<150&&y>40&&y<355)||(x>240&&x<315&&y>245&&y<560)
-				||(x>500&&x<550&&y>465&&y<670)||(x>500&&x<550&&y>145&&y<460)||(x>900&&x<950&&y>360&&y<670))) {
-			
-			
+		if((e.getKeyCode()==KeyEvent.VK_DOWN)&&((x>645&&x<680&&y>0&&y<158)||(x>245&&x<265&&y>158&&y<365)
+				||(x>495&&x<515&&y>365&&y<570)||(x>495&&x<515&&y>52&&y<260)||(x>895&&x<920&&y>260&&y<460))) {
 		
 			
 		
 			y=y+5;
 			
+		}
+		
+		if(e.getKeyCode()==KeyEvent.VK_ENTER){
+			
+			System.out.println(bullet.size());
+			String soundName = "C:\\Users\\Atiq\\eclipse-workspace\\ZoomBie Game\\res\\Gun+1.wav";    
+			AudioInputStream audioInputStream = null;
+			try {
+				audioInputStream = AudioSystem.getAudioInputStream(new File(soundName).getAbsoluteFile());
+			} catch (UnsupportedAudioFileException | IOException e1) {
+				
+				e1.printStackTrace();
+			}
+			Clip clip = null;
+			try {
+				clip = AudioSystem.getClip();
+			} catch (LineUnavailableException e1) {
+				e1.printStackTrace();
+			}
+			try {
+				clip.open(audioInputStream);
+			} catch (LineUnavailableException | IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			clip.start();
+			
+			shoot=true;
 		}
 		
 		
@@ -306,6 +491,38 @@ public class Zombie extends JPanel implements ActionListener,KeyListener  {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
+		
+		
+		
+		
+		
+		if(x==1075 && y==45 ) {
+			 
+			 
+			 
+			 
+	    	UIManager.put("OptionPane.messageFont", new FontUIResource(new Font( "Arial", Font.BOLD, 18)));       
+	    	UIManager.put("OptionPane.minimumSize",new Dimension(200,100));
+	    	JOptionPane.showMessageDialog(null, "Level Complete");
+	    	
+	    	
+	    	try        
+	    	{
+	    	    Thread.sleep(1000);
+	    	} 
+	    	catch(InterruptedException ex) 
+	    	{
+	    	    Thread.currentThread().interrupt();
+	    	}
+	    	
+	    	System.exit(0);
+	    	
+	    	//g.setVisible(false);
+	    
+	    	
+	    }
+		
+		
 		for(int i=0;i<30;i++) {
 		
 			if(new Rectangle(arrStar[i][0],arrStar[i][1],15,15).intersects(new Rectangle(x,y,40,60))&& arrStar[i][2]==1) {
@@ -314,10 +531,162 @@ public class Zombie extends JPanel implements ActionListener,KeyListener  {
 				
 				count++;
 				
+				String soundName = "C:\\Users\\Atiq\\eclipse-workspace\\ZoomBie Game\\res\\s.wav";    
+				AudioInputStream audioInputStream = null;
+				try {
+					audioInputStream = AudioSystem.getAudioInputStream(new File(soundName).getAbsoluteFile());
+				} catch (UnsupportedAudioFileException | IOException e1) {
+					
+					e1.printStackTrace();
+				}
+				Clip clip = null;
+				try {
+					clip = AudioSystem.getClip();
+				} catch (LineUnavailableException e1) {
+					e1.printStackTrace();
+				}
+				try {
+					clip.open(audioInputStream);
+				} catch (LineUnavailableException | IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				clip.start();
+				
 		
 				break;
 			}
-		}	
+		}
+		
+		
+		
+		for(int i=0;i<bullet.size();i++) {
+			
+			for(int j=0;j<arrZombie.length;j++) {
+				
+				 if(new Rectangle(bullet.get(i),y+15,15,5).intersects(new Rectangle(arrZombie[j][0],arrZombie[j][1],100,100))){
+					
+					
+					 
+					 String soundName = "C:\\Users\\Atiq\\eclipse-workspace\\ZoomBie Game\\res\\8.wav";    
+						AudioInputStream audioInputStream = null;
+						try {
+							audioInputStream = AudioSystem.getAudioInputStream(new File(soundName).getAbsoluteFile());
+						} catch (UnsupportedAudioFileException | IOException e1) {
+							
+							e1.printStackTrace();
+						}
+						Clip clip = null;
+						try {
+							clip = AudioSystem.getClip();
+						} catch (LineUnavailableException e1) {
+							e1.printStackTrace();
+						}
+						try {
+							clip.open(audioInputStream);
+						} catch (LineUnavailableException | IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+						clip.start();
+					 
+					 
+					 
+					 arrZombie[j][2]=0;
+					
+					
+					
+					
+				}
+				 else if(new Rectangle(arrZombie[j][0],arrZombie[j][1],20,20).intersects(new Rectangle(x,y,20,20)) && arrZombie[j][2]==1)
+				{
+					
+					
+					//System.out.println("bullet"+bullet.get(i)+"  "+(y+15)+"zombie"+arrZombie[j][0]+" "+arrZombie[j][1]);
+					
+					
+					 
+			    	UIManager.put("OptionPane.messageFont", new FontUIResource(new Font( "Arial", Font.BOLD, 18)));       
+			    	UIManager.put("OptionPane.minimumSize",new Dimension(200,100));
+			    	JOptionPane.showMessageDialog(null, "Loser");
+			    	
+			    	
+			    	try        
+			    	{
+			    	    Thread.sleep(1000);
+			    	} 
+			    	catch(InterruptedException ex) 
+			    	{
+			    	    Thread.currentThread().interrupt();
+			    	}
+			    	
+			    
+		    	
+					System.exit(0);
+					
+					
+				}
+			
+			
+			}
+		
+		}
+		
+		
+		
+		//zombie run
+		
+		arrZombie[0][0]+=dirValue[0];
+		arrZombie[1][0]-=dirValue[1];
+		arrZombie[2][0]-=dirValue[2];
+		arrZombie[3][0]+=dirValue[3];
+		arrZombie[4][0]+=dirValue[4];
+		arrZombie[5][0]+=dirValue[5];
+		
+		
+		
+		for(int i=0;i<6;i++) {
+			
+			if(arrZombie[i][0]>1200) {
+				
+				arrZombie[i][0]=1200;
+				
+				dirValue[i]=dirValue[i]*-1;
+				
+			}
+			
+			if(arrZombie[i][0]<0) {
+				
+				arrZombie[i][0]=0;
+				
+				dirValue[i]=dirValue[i]*-1;
+				
+			}
+			
+			
+		}
+		
+		
+		
+		
+		
+		//bullet run
+		
+			
+			for(int i=0;i<bullet.size();i++) {
+				
+				
+				if(bullet.get(i)>-20 ) bullet.set(i,bullet.get(i)-4);
+				
+			}
+			
+			
+			if(bullet.size()>40) bullet.removeAll(bullet);
+			
+		
+		
+		
+		
 			
 	
 		repaint();
